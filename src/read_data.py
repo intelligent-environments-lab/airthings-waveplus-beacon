5358f717c8bf8c8d876513bd68756da1b0c58a27
Hagen E. Fritz <hagenfritz@gmail.com>
2022-03-01 13:24:41 -0600

    BTLE error catch

--- src/read_data.py
+++ src/read_data.py
@@ -36,7 +36,7 @@
 # ===============================
 
 from xml.dom.minidom import parseString
-from bluepy.btle import UUID, Peripheral, Scanner, DefaultDelegate
+from bluepy.btle import UUID, Peripheral, Scanner, DefaultDelegate, BTLEManagementError
 import sys
 import time
 import struct
@@ -204,6 +204,10 @@ def main(SerialNumber):
                 log.info("Connected to AirThings")
                 log.info("--------------------------------------------------------------------------------------")
             except NameError as e:
+                log.warning(e)
+                # create temp file
+                data = get_error_data()
+            except BTLEManagementError as e:
                 log.warning(e)
                 # create temp file
                 data = get_error_data()
